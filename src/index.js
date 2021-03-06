6fe91e95eb0de9d019b1633694b8f41df1bd79a1
11Keng11 <keithngzk1111@gmail.com>
2021-03-06 22:21:12 +0800

    updated router with hash router

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import HomePage from './containers/Homepage.js';
 import ResultsPage from './containers/Resultspage.js';
 import FaqPage from './containers/Faqpage.js';
 import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 
 var hist = createBrowserHistory();
 unstable_createRoot(document.getElementById('root')).render(
-<Router history={hist}>
+<HashRouter history={hist}>
   <Switch>
     <Route path="/virtual-run/faq-page" component={FaqPage} />
     <Route path="/virtual-run/scoreboard-page" component={ResultsPage} />
     <Route path="/virtual-run" component={HomePage} />
   </Switch>
-</Router>);
+</HashRouter>);
 
 
